bf0015d62c48b58f860bceac1c8bc955d06ebdab
swapniljukariya <swapniljk14@gmail.com>
2025-03-27 01:37:11 +0530

    hello world2

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const bcrypt = require("bcryptjs");
 
 exports.registerUser = async (req, res) => {
   try {
-    const { name, email, password } = req.body;
+    const { name, username, email, password } = req.body;
 
     // Check if the user already exists
     const userExists = await User.findOne({ email });
@@ -15,8 +15,8 @@ exports.registerUser = async (req, res) => {
     // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create user with hashed password
-    const user = await User.create({ name, email, password: hashedPassword });
+    // Create user with hashed password and username
+    const user = await User.create({ name, username, email, password: hashedPassword });
 
     // Generate JWT token
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
@@ -28,6 +28,7 @@ exports.registerUser = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
 
 exports.loginUser = async (req, res) => {
   try {
